e4a0538b2f57b58058430bdb4a72bc2f225fb59a
koraykoca <koraykoca34@gmail.com>
2022-03-12 21:47:28 +0100

    multiply and divide added

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+
+double a{0.0};
+double b{0.0};
+
+double multiply(double x, double y){
+    return x*y;
+}
+
+double divide(double x, double y){
+    if (y != 0){
+        return x / y;
+    }
+    else{
+        std::cout << "Division by zero" << '\n';
+        return 0.0;
+    }
+}
+
 int main(){
     
 }
